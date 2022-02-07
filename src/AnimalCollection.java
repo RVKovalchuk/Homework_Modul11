d74776eaedce07e1538d79ae4c12cc3cd25bc3aa
RV.Kovalchuk <rv.kovalchuk@tut.by>
2022-02-08 01:57:07 +0300

    Added IllegalArgumentException for the constructor.

--- src/AnimalCollection.java
+++ src/AnimalCollection.java
@@ -2,45 +2,63 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class AnimalCollection {
+public class AnimalCollection{
+    //создаем hashMap c default initial capacity.
     private static final Map<Integer, AnimalCollection> animalList = new HashMap<>();
-
+    //объявляем необходимые для конструктора переменные.
     private final String speciesOfAnimal;
     private final String name;
-    private final int age;
-    private final double weight;
-
+    private int age;
+    private double weight;
+    //добавляем getter для возможности получения отдельной информации об экземпляре класса.
+    public String getSpeciesOfAnimal() {
+        return speciesOfAnimal;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    //добавляем setter для возможности изменения переменной информации об экземплярах класса.
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    //создаем конструктор.
     public AnimalCollection(String speciesOfAnimal, String name, int age, double weight) {
+        //выдаем исключение при вводе отрицательного возраста или отрицательного веса.
+        if (age <= 0 || weight <= 0.0) {
+            throw new IllegalArgumentException();
+        }
         this.speciesOfAnimal = speciesOfAnimal;
         this.name = name;
         this.age = age;
         this.weight = weight;
+        //введеный экземпляр класса кладем в коллекцию.
         animalList.put(setId(), this);
     }
-
-    public int setId() {
+    //рассчитываем уникальный id-ключ для экземпляра класса в коллекции.
+    private int setId() {
         return animalList.size() + 1;
     }
-
+    //создаем метод для возврата всей информации об экземпляре класса.
     public String getAllInfo() {
         return "[species of animal: " + speciesOfAnimal + ", name: " + name + ", age: " + age +
                 ", weight: " + weight + ']';
     }
-
+    //создаем метод для вывода всей коллекции.
     public static void printList() {
         for (Map.Entry<Integer, AnimalCollection> map : animalList.entrySet()) {
-            System.out.printf("Id: %s - %s \n", map.getKey(),  map.getValue().getAllInfo());
+            System.out.printf("Id: %s - %s \n", map.getKey(), map.getValue().getAllInfo());
         }
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AnimalCollection that = (AnimalCollection) o;
-        return Objects.equals(speciesOfAnimal, that.speciesOfAnimal) && Objects.equals(name, that.name);
-    }
-
+    //переопределяем метод hashCode().
     @Override
     public int hashCode() {
         return Objects.hash(speciesOfAnimal, name);
--- src/App_AnimalCollection.java
+++ src/App_AnimalCollection.java
@@ -1,14 +1,17 @@
 public class App_AnimalCollection {
     public static void main(String[] args) {
-
+        //создаем экземпляры классов.
         AnimalCollection CatStinky = new AnimalCollection("Cat", "Stinky", 12, 8.4);
         AnimalCollection DogFluffy = new AnimalCollection("Dog", "Fluffy", 3, 26);
         AnimalCollection CatCorry = new AnimalCollection("Cat", "Cory", 2, 2.3);
         AnimalCollection DogLazy = new AnimalCollection("Dog", "Lazy", 5, 33.4);
         AnimalCollection RatHippo = new AnimalCollection("Rat", "Hippo", 1, 0.7);
         AnimalCollection CatLuna = new AnimalCollection("Cat", "Luna", 9, 5.0);
-
+        //выводим информацию об отдельном экземпляре.
+        System.out.println(DogFluffy.getAllInfo());
+        //меняем информацию об отдельном экземпляре.
+        CatStinky.setWeight(9.2);
+        //выводим всю коллекцию.
         AnimalCollection.printList();
-
     }
 }
